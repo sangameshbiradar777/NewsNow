25e80f8c034c2cf0561534d8bf483bb0f9383267
Sangamesh <sangameshbiradar586141@gmail.com>
2022-10-24 23:01:11 +0530

    Netlify Integration

--- frontend/modules/homepage/helper.js
+++ frontend/modules/homepage/helper.js
@@ -1,23 +1,23 @@
-import topHeadlines from "../../../backend/top-headlines.js";
+// import topHeadlines from "../../../backend/top-headlines.js";
 
 // Implementation of the function fetchTrendingNews
 async function fetchNews(newsURL) {
-  // try {
-  //   const newsURLResponse = await fetch(newsURL);
+  try {
+    const newsURLResponse = await fetch(newsURL);
 
-  //   if (newsURLResponse.ok) {
-  //     const news = await newsURLResponse.json();
-  //     return news;
-  //   } else {
-  //     const errorMessage = `⚡⚡An unknown error occurred with a status code of ${trendingNewsURLResponse.status}⚡⚡`;
-  //     throw new Error(errorMessage);
-  //   }
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
+    if (newsURLResponse.ok) {
+      const news = await newsURLResponse.json();
+      return news;
+    } else {
+      const errorMessage = `⚡⚡An unknown error occurred with a status code of ${trendingNewsURLResponse.status}⚡⚡`;
+      throw new Error(errorMessage);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 
   // TEMPORARY
-  return topHeadlines;
+  // return topHeadlines;
 }
 
 export { fetchNews };
